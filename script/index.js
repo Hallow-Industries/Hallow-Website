a1aed7cbe5cf9c8991b13d840d76dff8b2f5e941
Yochran <71285258+Yochran@users.noreply.github.com>
2021-03-30 20:44:25 -0400

    Add files via upload

--- script/index.js
+++ script/index.js
@@ -1,9 +1,19 @@
 const background = document.getElementById("background");
+const navbar = document.getElementById("nav-bar");
+const navbar2 = document.getElementById("nav-bar-2");
 
 // Functions
 function parallax() {
-    background.style.top = Math.floor(-20 - 0.195 * window.scrollY) + "px";
-    background.style.filter = "blur(" + Math.floor(3 + 0.0195 * window.scrollY) + "px)"
+    background.style.top = Math.floor(-20 - 0.2575 * window.scrollY) + "px";
+    background.style.filter = "blur(" + Math.floor(5 + 0.02575 * window.scrollY) + "px)"
+
+    var offset = navbar.offsetTop;
+
+    if (window.pageYOffset >= offset) {
+        navbar2.style.top = "0.05%";
+    } else {
+        navbar2.style.top = "125%";
+    }
 }
 // Listeners
 document.onscroll = () => { parallax(); }
