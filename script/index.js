744dab496b542131133ca298b3fe0745c1a4add0
Yochran <71285258+Yochran@users.noreply.github.com>
2021-04-01 15:56:49 -0400

    Add files via upload

--- script/index.js
+++ script/index.js
@@ -1,8 +1,11 @@
 const navbar = document.getElementById("nav-bar");
 const backsheet = document.getElementById("backsheet");
+const title = document.getElementById("title");
 
 function scrollEffects() {
     var offset = backsheet.offsetTop;
+
+    title.style.top = Math.round(37.5 - 0.17 * window.scrollY) + "%";
 
     if (window.pageYOffset >= offset - 65) {
         navbar.style.backgroundColor = "#111";
